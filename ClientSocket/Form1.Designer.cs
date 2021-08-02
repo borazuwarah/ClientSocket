242d6bb5247da8d1e67de56a0e625f3fb145895a
Pablo Ramírez <yeyoycareto@gmail.com>
2021-08-02 22:53:17 +0200

    proyect client

---
+++ ClientSocket/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ClientSocket
+{
+    partial class ClientSocket
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTextToServer = new System.Windows.Forms.TextBox();
+            this.btEnviar = new System.Windows.Forms.Button();
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtTextToServer
+            // 
+            this.txtTextToServer.Location = new System.Drawing.Point(45, 201);
+            this.txtTextToServer.Multiline = true;
+            this.txtTextToServer.Name = "txtTextToServer";
+            this.txtTextToServer.Size = new System.Drawing.Size(405, 66);
+            this.txtTextToServer.TabIndex = 0;
+            // 
+            // btEnviar
+            // 
+            this.btEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEnviar.Location = new System.Drawing.Point(130, 84);
+            this.btEnviar.Name = "btEnviar";
+            this.btEnviar.Size = new System.Drawing.Size(234, 83);
+            this.btEnviar.TabIndex = 1;
+            this.btEnviar.Text = "Send to Server";
+            this.btEnviar.UseVisualStyleBackColor = true;
+            this.btEnviar.Click += new System.EventHandler(this.btEnviar_Click);
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AccessibleRole = System.Windows.Forms.AccessibleRole.Sound;
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.lbTitle.Location = new System.Drawing.Point(183, 9);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(128, 46);
+            this.lbTitle.TabIndex = 2;
+            this.lbTitle.Text = "Client";
+            // 
+            // ClientSocket
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(522, 332);
+            this.Controls.Add(this.lbTitle);
+            this.Controls.Add(this.btEnviar);
+            this.Controls.Add(this.txtTextToServer);
+            this.MaximizeBox = false;
+            this.Name = "ClientSocket";
+            this.Text = "Client Socket";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTextToServer;
+        private System.Windows.Forms.Button btEnviar;
+        private System.Windows.Forms.Label lbTitle;
+    }
+}
+
---
+++ ClientSocket/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace ClientSocket
+{
+    public partial class ClientSocket : Form
+    {
+        public ClientSocket()
+        {
+            InitializeComponent();
+        }
+
+        private void btEnviar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtTextToServer.Text))
+                MessageBox.Show("Please enter a message to continue ", "Message error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                //conection
+                Socket listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                IPEndPoint connect = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6400);
+                try
+                {
+                    listen.Connect(connect);
+                    byte[] sendData = new byte[100];
+                    string data;
+
+                    data = txtTextToServer.Text;
+                    sendData = Encoding.Default.GetBytes(data);
+
+                    listen.Send(sendData);
+                    //listen.Disconnect(true);
+                    listen.Close();
+                    // listen.Dispose();
+                }
+                catch (SocketException exception)
+                {
+                    //MessageBox.Show("Error: "+ exception.ToString());
+
+                    MessageBox.Show("No active service detected in IP:" + connect.Address + " and port: " + connect.Port + "\n Please activate the server previously to continue",
+                        "Server Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+    }
+}
